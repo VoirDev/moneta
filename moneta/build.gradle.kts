621ad755a2cfa6c51bfe8182adba9c8989bbcd4c
Gary Bezruchko <gary.bezruchko@gmail.com>
2025-11-27 20:47:33 +0300

    Initial release

--- moneta/build.gradle.kts
+++ moneta/build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     id("maven-publish")
-    id("signing")
 }
 
 group = "dev.voir"
-version = "1.0.0-alpha01"
+version = "0.0.1"
 
 kotlin {
     jvmToolchain(21)
@@ -14,28 +13,39 @@ kotlin {
         freeCompilerArgs.add("-opt-in=kotlin.time.ExperimentalTime")
         freeCompilerArgs.add("-Xexpect-actual-classes")
     }
-}
 
-
-kotlin {
     jvm()
     iosX64()
     iosArm64()
     iosSimulatorArm64()
-    
+
     sourceSets {
-        jvmMain.dependencies {
-
-        }
-
-        commonMain.dependencies {
-        }
-
-        iosMain.dependencies {
-        }
-
+        commonMain.dependencies { }
         commonTest.dependencies {
             implementation(kotlin("test"))
+        }
+        jvmMain.dependencies { }
+        iosMain.dependencies { }
+    }
+}
+
+publishing {
+    publications.withType<MavenPublication>().configureEach {
+        pom {
+            name.set("moneta")
+            description.set("Moneta – Kotlin Multiplatform money type")
+            url.set("https://github.com/VoirDev/moneta")
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/VoirDev/moneta")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
         }
     }
 }
